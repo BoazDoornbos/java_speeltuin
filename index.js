93030b6e1fa79e17a8ea28d0707ecd458c904c05
Boaz Doornbos <84346524+BoazDoornbos@users.noreply.github.com>
2021-06-26 22:14:42 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hello winc Academy")
+let boaz = "boaz"
+console.log(boaz)
+numbers = 4 + 4
+ strings = "4" + "4"
+console.log(numbers)
+console.log(strings)
+addition = 2 + 2
+ subtraction = 3 - 2
+multiplication = 6 * 2
+ division =  1 / 2
+modulus =  21 % 2
+ console.log("addition")
+console.log(addition)
+ console.log("Subtraction")
+console.log(subtraction)
+ console.log("Multiplication")
+console.log(multiplication)
+ console.log("Division")
+console.log(division)
+ console.log("Modulus")
+console.log(modulus)
+ console.log(typeof 48)
+console.log(typeof "48")
